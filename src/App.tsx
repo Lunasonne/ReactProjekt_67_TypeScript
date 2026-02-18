db06d5ecbbbdfdeac08f450a2c91b084ae5a422d
Natalija <Lunasonne37@outlook.com>
2026-02-18 15:36:22 +0100

    Lesson_06_TypeScript_types

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+ import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
+
+function App() {
+   
+    return <> 
+    {/* Homeworks */}
+   {/* Lessons  */}
+<Lesson_06/>
+    </>
+}
+
+export default App
